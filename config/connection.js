b9a985fe4bf3f35d5265172c97fd0783ed1e35bd
Kristina Calumpong <kcalumpong@gmail.com>
2019-09-21 18:10:12 -0700

    final burger app

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+      var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+      });
+};
 
 connection.connect(function(err) {
   if (err) {
@@ -14,6 +18,7 @@ connection.connect(function(err) {
     return;
   }
   console.log("connected as id " + connection.threadId);
+  
 });
 
 module.exports = connection;
